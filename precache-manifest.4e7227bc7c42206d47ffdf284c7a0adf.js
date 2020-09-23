476f294dd48fe1478204359fce13023281c43890
Anando <anandoghose@gmail.com>
2020-09-23 10:58:25 +0530

    Updates

---
+++ precache-manifest.4e7227bc7c42206d47ffdf284c7a0adf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff6487e40b76a953300e8c2ec253fea0",
+    "url": "/solitude-react/index.html"
+  },
+  {
+    "revision": "4b22a7c9d4bf00bdff15",
+    "url": "/solitude-react/static/css/2.f278118f.chunk.css"
+  },
+  {
+    "revision": "c1d698b180fb6bf1558d",
+    "url": "/solitude-react/static/css/main.3765f19c.chunk.css"
+  },
+  {
+    "revision": "4b22a7c9d4bf00bdff15",
+    "url": "/solitude-react/static/js/2.ffd4bf50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/solitude-react/static/js/2.ffd4bf50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1d698b180fb6bf1558d",
+    "url": "/solitude-react/static/js/main.667330ce.chunk.js"
+  },
+  {
+    "revision": "41b30ba9af5780cc103e",
+    "url": "/solitude-react/static/js/runtime-main.66163a2b.js"
+  },
+  {
+    "revision": "e59a7b241cc7a49d463b7e5b95c60082",
+    "url": "/solitude-react/static/media/logo.e59a7b24.svg"
+  },
+  {
+    "revision": "c1f2a59ae1e363aee2d163450d1aa372",
+    "url": "/solitude-react/static/media/our_story.c1f2a59a.svg"
+  },
+  {
+    "revision": "39bef645bb626c232b67848e66da0998",
+    "url": "/solitude-react/static/media/papaya.39bef645.jpg"
+  }
+]);
