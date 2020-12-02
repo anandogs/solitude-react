4746ec0afe0d39c730991e21c62d92a9b912abd0
Anando <anandoghose@gmail.com>
2020-12-02 18:24:56 +0530

    Updates

---
+++ precache-manifest.e966cf085f06476bac27add2f63d2dfa.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97c1e6a63b25da46e2f824eb836b2f82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "478184cdfd8f4cbc5e53",
+    "url": "/static/css/2.f278118f.chunk.css"
+  },
+  {
+    "revision": "ae8eac5d0efcab1f9329",
+    "url": "/static/css/main.bab2415d.chunk.css"
+  },
+  {
+    "revision": "478184cdfd8f4cbc5e53",
+    "url": "/static/js/2.895f3e90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.895f3e90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae8eac5d0efcab1f9329",
+    "url": "/static/js/main.bfc9be3a.chunk.js"
+  },
+  {
+    "revision": "31576bb872c43e360d59",
+    "url": "/static/js/runtime-main.0a73cb38.js"
+  },
+  {
+    "revision": "b6a42d2f0f66f1f40f1b2272d549f505",
+    "url": "/static/media/banner.b6a42d2f.png"
+  },
+  {
+    "revision": "4424da065fc02af42208e99d2fc404b0",
+    "url": "/static/media/bg.4424da06.png"
+  },
+  {
+    "revision": "5a9eb99a415a623f5ccb3557eaf5c650",
+    "url": "/static/media/bgImg1.5a9eb99a.jpg"
+  },
+  {
+    "revision": "519c23566a3c3ab42303a2099ea87aab",
+    "url": "/static/media/eat-local-mob.519c2356.jpg"
+  },
+  {
+    "revision": "900a1f0273a4cf03ac4ba5d8f2968fbc",
+    "url": "/static/media/eat-local.900a1f02.jpg"
+  },
+  {
+    "revision": "f865a76bf22fbbfcc17a03e77676eef4",
+    "url": "/static/media/fullpage.f865a76b.jpg"
+  },
+  {
+    "revision": "e59a7b241cc7a49d463b7e5b95c60082",
+    "url": "/static/media/logo.e59a7b24.svg"
+  },
+  {
+    "revision": "e1d4ffaaf55e7af81ee423db7677af97",
+    "url": "/static/media/perma-mob.e1d4ffaa.jpg"
+  },
+  {
+    "revision": "41f24975bc434786ec4765ec4dcb443c",
+    "url": "/static/media/perma.41f24975.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/solitude-react/precache-manifest.d9836482ed2de9b5341e51d443d57eaf.js"
+  "/precache-manifest.e966cf085f06476bac27add2f63d2dfa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/solitude-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
